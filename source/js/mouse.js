128374f3ca3f2127d1b0680a8d5c9240d33c68d3
liak0 <2294760573@qq.com>
2020-04-25 11:45:26 +0800

    修复了鼠标点击bug

--- source/js/mouse.js
+++ source/js/mouse.js
@@ -1,19 +1,71 @@
-(function() {
-    var coreSocialistValues = ["富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善"],
-    index = Math.floor(Math.random() * coreSocialistValues.length);
-	
-    document.body.addEventListener('click',
-    function(e) {
-	//过滤a标签
-        if (e.target.tagName == 'A') {
-            return;
-        }
-        var x = e.pageX,
-        y = e.pageY,
-        span = document.createElement('span');
-        span.textContent = coreSocialistValues[index];
-        index = (index + 1) % coreSocialistValues.length;
-        span.style.cssText = ['z-index: 9999999; position: absolute; font-weight: bold; color: #ff6651; top: ', y - 20, 'px; left: ', x, 'px;'].join('');
-        document.body.appendChild(span);
-        animate(span);
-    });
+onload = function() {
+    var click_cnt = 0;
+    var $html = document.getElementsByTagName("html")[0];
+    var $body = document.getElementsByTagName("body")[0];
+    $html.onclick = function(e) {
+    var $elem = document.createElement("b");
+    $elem.style.color = "#E94F06";
+    $elem.style.zIndex = 9999;
+    $elem.style.position = "absolute";
+    $elem.style.select = "none";
+    var x = e.pageX;
+    var y = e.pageY;
+    $elem.style.left = (x - 10) + "px";
+    $elem.style.top = (y - 20) + "px";
+    clearInterval(anim);
+    switch (++click_cnt) {
+     case 3:
+     $elem.innerText = "OωO";
+     break;
+     case 5:
+     $elem.innerText = "(๑•́ ∀ •̀๑)";
+     break;
+     case 7:
+     $elem.innerText = "(๑•́ ₃ •̀๑)";
+     break;
+     case 9:
+     $elem.innerText = "(๑•̀_•́๑)";
+     break;
+     case 11:
+     $elem.innerText = "（￣へ￣）";
+     break;
+     case 13:
+     $elem.innerText = "(╯°口°)╯(┴—┴";
+     break;
+     case 17:
+     $elem.innerText = "૮( ᵒ̌皿ᵒ̌ )ა";
+     break;
+     case 19:
+     $elem.innerText = "╮(｡>口<｡)╭";
+     break;
+     case 21:
+     $elem.innerText = "( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃";
+     break;
+     case 100:
+     case 101:
+     case 102:
+     case 103:
+     case 104:
+     case 105:
+     $elem.innerText = "(ꐦ°᷄д°᷅)";
+     break;
+     default:
+     $elem.innerText = "❤";
+     break;
+    }
+    $elem.style.fontSize = Math.random() * 10 + 8 + "px";
+    var increase = 0;
+    var anim;
+    setTimeout(function() {
+     anim = setInterval(function() {
+     if (++increase == 150) {
+      clearInterval(anim);
+      $body.removeChild($elem);
+     }
+     $elem.style.top = y - 20 - increase + "px";
+     $elem.style.opacity = (150 - increase) / 120;
+     }, 8);
+    }, 70);
+    $body.appendChild($elem);
+    };
+   };
